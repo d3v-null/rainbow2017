0754e764d23c8d153f63b644dfc0d2f0c3d9fc88
derwentx <derwentx@gmail.com>
2017-03-04 19:11:46 +1100

    added double rainbow

---
+++ double rainbow/double_rainbow/double_rainbow.ino
@@ -0,0 +1,55 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN1 6
+#define PIN2 7
+#define NUMPIXELS 16
+
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(NUMPIXELS, PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  strip1.begin();
+  strip1.show(); // Initialize all pixels to 'off'
+  strip2.begin();
+  strip2.show();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  rainbow(20);
+}
+
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<NUMPIXELS; i++) {
+      strip1.setPixelColor(i, Wheel((i+j) & 255));
+      strip2.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip1.show();
+    strip2.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip1.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip1.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip1.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
